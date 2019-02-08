0570086916f957f303ea1f4019f2d78cd3ab75cb
SBG289 <46947537+SBG289@users.noreply.github.com>
2019-02-07 23:30:01 -0500

    Add files via upload

---
+++ Factorial_Application.cpp
@@ -0,0 +1,60 @@
+// Factorial_Application.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// C++ Program to find the 
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+void factorial()
+{
+	int x, i;
+	double f = 1;
+
+	
+		cout << "Input Factorial: ";
+		cin >> x;
+		cout << endl;
+		f = x;
+
+		while (x > 1)
+		{
+			x--;
+			f = f * x;
+		}
+
+		cout << "Factorial is equal to: " << f << endl;
+		cout << endl;
+
+		/*for (i = 1; i <= x; i++)
+		{
+			f = f * i;
+		}
+		cout << endl;
+		cout << "Factorial is equal to: " << f << endl;
+
+		if (x = 0) break;*/
+	
+
+}
+
+	int main()
+	{
+
+		char c;
+
+		while (true)
+		{
+			factorial();
+
+			cout << "Would you like to run the program again? y/n: ";
+			cin >> c;
+			cout << endl;
+
+			if (c == 'n' || c == 'N' || c == '0') break;
+
+			cout << endl << "====================================================" << endl << endl;
+			
+		}
+		return 0;
+	}
